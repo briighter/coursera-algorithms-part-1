bd65e2c8ed4cf6197cd17f755771212324416a8e
Jason <limaeight@gmail.com>
2022-11-10 19:57:28 -0500

    Initial commit

--- week1/src/com/algorithmspart1/RandomWord.java
+++ week1/src/com/algorithmspart1/RandomWord.java
@@ -1,7 +1,12 @@
 package com.algorithmspart1;
 
+import static edu.princeton.cs.algs4.StdIn.readString;
+
 public class RandomWord {
     public static void main(String[] args) {
-        
+        // Write a program RandomWord.java that (1) reads a sequence of words from standard input
+        // and (2) prints one of those words uniformly at random.
+        // Do not store the words in an array or list. Instead, use Knuth’s method: when reading the ith word, select it with probability 1/i to be the champion, replacing the previous champion. After reading all of the words, print the surviving champion.
+        readString();
     }
 }
